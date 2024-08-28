f274646f46c3f500dfc9c3a5eb835aaeb7f9b575
yasminesdj <yasminesaidj319@gmail.com>
2024-08-28 19:25:18 +0100

    adding the snake and the food

--- snake.js
+++ snake.js
@@ -4,6 +4,14 @@ var row =20;
 var col = 20;
 var board;
 var context;
+//snake 
+var x = blockSize * 4;
+var y = blockSize * 4;
+//food
+var fx = blockSize * 10;
+var fy = blockSize * 10;
+
+
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -15,4 +23,10 @@ window.onload = function() {
 function update(){
     context.fillStyle="black"; //change the color of the pen to black
     context.fillRect(0, 0, board.width, board.height); //filling 500 (25*20)
+
+    context.fillStyle= "green" //the color of the snake 
+    context.fillRect(x,y, blockSize, blockSize); // coordinates + width+height 
+
+    context.fillStyle= "red"; // the color of the food 
+    context.fillRect(fx , fy , blockSize, blockSize);
 }
