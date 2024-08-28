8ba25f8a78a515b04f05a25a5ad4ddadfee3c401
yasminesdj <yasminesaidj319@gmail.com>
2024-08-28 19:47:00 +0100

    create direction function

--- snake.js
+++ snake.js
@@ -7,6 +7,8 @@ var context;
 //snake 
 var x = blockSize * 4;
 var y = blockSize * 4;
+var speedX = 0;
+var speedy = 0;
 //food
 var fx ;
 var fy ;
@@ -19,6 +21,7 @@ window.onload = function() {
     board.width = col * blockSize;
     context = board.getContext("2d"); // to draw on the board
     Food(); // to replace the food randomly every time while refresh
+    document.addEventListener("keyup" , Direction);
     update();
 }
 function update(){
@@ -30,6 +33,25 @@ function update(){
 
     context.fillStyle= "red"; // the color of the food 
     context.fillRect(fx , fy , blockSize, blockSize);
+}
+function Direction(event) {
+    if (event.code == "ArrowUp"){ //if you go up
+        speedX = 0;
+        speedY = -1;
+
+    }
+   else if (event.code == "Arrowleft"){ //if you go left
+        speedX = -1;
+        speedY = 0;
+
+    }
+    if (event.code == "Arrowright"){ //if you go right
+        speedX = 1;
+        speedY = 0;
+
+    }
+   
+
 }
 
 function Food() {
